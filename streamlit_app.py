f4e3f89d13e39ff38c87849956bf38a31ab68a0e
OmniGnosis <123162031+editionhm@users.noreply.github.com>
2024-09-30 00:33:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -136,7 +136,7 @@ if st.session_state.user["connected"]:
         with st.chat_message("assistant"):
             with st.spinner("Thinking..."):
 
-                prompt = f""" You are an expert teacher. Elaborate on the following subject in the following text: {prompt}. You need to be engaging and understandable. Provide a clear explanation with examples if possible.
+                prompt = f""" You are an expert Arabic Grammar teacher. Elaborate on the following subject in the following chapter : {selected_chapter}. You need to be engaging and understandable. Provide a clear explanation with examples if possible.
                 """
                 #response = interact.generate_llm(prompt)
                 response = iot_module.run_iot(iot, prompt)
