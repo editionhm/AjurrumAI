260b1b159dba57829e8398f3f0564b293e6dc517
OmniGnosis <123162031+editionhm@users.noreply.github.com>
2024-10-27 22:34:01 +0100

    Update and rename 1-Tutorial.py to 1-Poem Generation.py

--- pages/1-Poem Generation.py
+++ pages/1-Poem Generation.py
@@ -1,10 +1,8 @@
 import utils
 import streamlit as st
-from streaming import StreamHandler
 
-from langchain.chains import ConversationChain
 
-st.set_page_config(page_title="Chatbot", page_icon="💬")
+st.set_page_config(page_title="Poem Generation", page_icon="💬")
 st.header('Basic Chatbot')
 st.write('Allows users to interact with the LLM')
 st.write('[![view source code ](https://img.shields.io/badge/view_source_code-gray?logo=github)](https://github.com/shashankdeshpande/langchain-chatbot/blob/master/pages/1_%F0%9F%92%AC_basic_chatbot.py)')
