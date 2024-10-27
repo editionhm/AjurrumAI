cbb3536e24c413b4b97d35853e9d0b143fded2cb
OmniGnosis <123162031+editionhm@users.noreply.github.com>
2024-10-27 22:35:50 +0100

    Update 3-إعراب tool.py

--- pages/3-إعراب tool.py
+++ pages/3-إعراب tool.py
@@ -1,1 +1,11 @@
 
+import utils
+import streamlit as st
+
+
+st.set_page_config(page_title="Poem Generation", page_icon="💬")
+st.header('Basic Chatbot')
+st.write('Allows users to interact with the LLM')
+st.write('[![view source code ](https://img.shields.io/badge/view_source_code-gray?logo=github)](https://github.com/shashankdeshpande/langchain-chatbot/blob/master/pages/1_%F0%9F%92%AC_basic_chatbot.py)')
+
+st.write("POEM GENERATION")
