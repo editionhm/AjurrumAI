d2398a8adf21007d53c5c91965333b104e2f1cbc
OmniGnosis <123162031+editionhm@users.noreply.github.com>
2024-10-28 20:17:29 +0100

    Update and rename 3-irab tool.py to 3-irab.py

--- pages/3-irab.py
+++ pages/3-irab.py
@@ -3,9 +3,9 @@ import utils
 import streamlit as st
 
 
-st.set_page_config(page_title="Poem Generation", page_icon="💬")
+st.set_page_config(page_title="IRAB TOOL", page_icon="💬")
 st.header('Basic Chatbot')
 st.write('Allows users to interact with the LLM')
 st.write('[![view source code ](https://img.shields.io/badge/view_source_code-gray?logo=github)](https://github.com/shashankdeshpande/langchain-chatbot/blob/master/pages/1_%F0%9F%92%AC_basic_chatbot.py)')
 
-st.write("POEM GENERATION")
+st.write("IRAB TOOL")
